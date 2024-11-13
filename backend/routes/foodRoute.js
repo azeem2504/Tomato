7fbc4508e487f5c8123014f635c02378fb6b067b
azeem2504 <118335209+azeem2504@users.noreply.github.com>
2024-11-13 15:30:35 +0530

    Update foodRoute.js

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { addFood, listFood, removeFood } from "../controllers/foodController.js";
 import multer from "multer";
+import path from "path"; 
 
 const foodRouter = express.Router()
 
